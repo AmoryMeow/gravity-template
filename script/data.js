96ad3978d44f8f9c3d62be3bccfafc9c93e9553d
Amory <garbage90@yandex.ru>
2020-09-07 08:06:52 +0300

    slider_js_step1

---
+++ script/data.js
@@ -0,0 +1,22 @@
+sliderPhoto = [
+  {
+    img: './images/slider_image.jpg',
+    link: "#",
+    video: true
+  },
+  {
+    img: './images/slider_image_disable.jpg',
+    link: '#',
+    video: false
+  },
+  {
+    img: './images/slider_image.jpg',
+    link: "#",
+    video: true
+  },
+  {
+    img: './images/slider_image_disable.jpg',
+    link: "#",
+    video: true
+  }
+]
